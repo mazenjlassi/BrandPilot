2e9a8ffffa2da6daf525b492d59eaab31f5f8dff
mazenjlassi <jlmazen1073@gmail.com>
2026-06-08 19:14:02 +0100

    fix: use @Bean method parameter for Environment instead of constructor injection

--- backend/src/main/java/com/example/metatry/Config/DataSourceConfig.java
+++ backend/src/main/java/com/example/metatry/Config/DataSourceConfig.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
@@ -11,19 +12,18 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-    private final Environment env;
-
-    public DataSourceConfig(Environment env) {
-        this.env = env;
-    }
-
     @Bean
-    public DataSource dataSource() {
+    @Primary
+    public DataSource dataSource(Environment env) {
+        String url = env.getProperty("spring.datasource.url");
+        String username = env.getProperty("spring.datasource.username");
+        String password = env.getProperty("spring.datasource.password");
+        String driverClassName = env.getProperty("spring.datasource.driver-class-name");
         return DataSourceBuilder.create()
-                .url(env.getProperty("spring.datasource.url"))
-                .username(env.getProperty("spring.datasource.username"))
-                .password(env.getProperty("spring.datasource.password"))
-                .driverClassName(env.getProperty("spring.datasource.driver-class-name"))
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName(driverClassName)
                 .type(HikariDataSource.class)
                 .build();
     }
