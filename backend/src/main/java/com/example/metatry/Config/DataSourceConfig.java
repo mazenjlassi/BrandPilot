1c3f594b0ece0c36db740a9278e306c9dc628c88
mazenjlassi <jlmazen1073@gmail.com>
2026-06-08 19:51:02 +0100

    fix: pass datasource props as -D system properties in CI; use Environment in DataSourceConfig

--- backend/src/main/java/com/example/metatry/Config/DataSourceConfig.java
+++ backend/src/main/java/com/example/metatry/Config/DataSourceConfig.java
@@ -1,13 +1,11 @@
 package com.example.metatry.Config;
 
-import java.io.InputStream;
-import java.util.Properties;
-
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
@@ -16,42 +14,12 @@ public class DataSourceConfig {
 
     @Bean
     @Primary
-    public DataSource dataSource() {
-        Properties props = new Properties();
-        ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        String[] paths = {
-            "application.properties",
-            "/application.properties",
-            "BOOT-INF/classes/application.properties",
-            "classpath:application.properties"
-        };
-        Exception lastEx = null;
-        for (String path : paths) {
-            try (InputStream is = cl.getResourceAsStream(path)) {
-                if (is != null) {
-                    props.load(is);
-                    break;
-                }
-            } catch (Exception e) {
-                lastEx = e;
-            }
-        }
-        if (props.isEmpty()) {
-            StringBuilder sb = new StringBuilder("Cannot find application.properties. Tried: ");
-            for (String p : paths) sb.append(p).append(" ");
-            sb.append(" CL=").append(cl);
-            if (lastEx != null) sb.append(" ex=").append(lastEx);
-            throw new RuntimeException(sb.toString());
-        }
-        String url = props.getProperty("spring.datasource.url");
-        String username = props.getProperty("spring.datasource.username");
-        String password = props.getProperty("spring.datasource.password");
-        String driverClassName = props.getProperty("spring.datasource.driver-class-name");
+    public DataSource dataSource(Environment env) {
         return DataSourceBuilder.create()
-                .url(url)
-                .username(username)
-                .password(password)
-                .driverClassName(driverClassName)
+                .url(env.getProperty("spring.datasource.url"))
+                .username(env.getProperty("spring.datasource.username"))
+                .password(env.getProperty("spring.datasource.password"))
+                .driverClassName(env.getProperty("spring.datasource.driver-class-name"))
                 .type(HikariDataSource.class)
                 .build();
     }
