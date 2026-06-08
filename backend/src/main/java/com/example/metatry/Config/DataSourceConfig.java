5544856845f4d851916b1db6e3d4573a5ec521df
mazenjlassi <jlmazen1073@gmail.com>
2026-06-08 19:29:09 +0100

    debug: single-line env info for CI visibility

--- backend/src/main/java/com/example/metatry/Config/DataSourceConfig.java
+++ backend/src/main/java/com/example/metatry/Config/DataSourceConfig.java
@@ -17,21 +17,20 @@ public class DataSourceConfig {
     @Bean
     @Primary
     public DataSource dataSource(Environment env) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("=== DataSourceConfig DEBUG ===\n");
-        sb.append("Environment: ").append(env).append('\n');
+        StringBuilder sb = new StringBuilder("=== DataSourceConfig DEBUG === | ");
+        sb.append("env=").append(env.getClass().getName()).append(" | ");
         if (env instanceof ConfigurableEnvironment) {
             ConfigurableEnvironment ce = (ConfigurableEnvironment) env;
-            sb.append("PropertySources:\n");
+            sb.append("ps_count=").append(ce.getPropertySources().size()).append(" | ");
             for (PropertySource<?> ps : ce.getPropertySources()) {
                 Object val = ps.getProperty("spring.datasource.url");
-                sb.append("  ").append(ps.getName()).append(" -> ").append(val).append('\n');
+                sb.append("[").append(ps.getName()).append("=").append(val).append("] ");
             }
         }
-        sb.append("url=").append(env.getProperty("spring.datasource.url")).append('\n');
-        sb.append("username=").append(env.getProperty("spring.datasource.username")).append('\n');
-        sb.append("password=").append(env.getProperty("spring.datasource.password")).append('\n');
-        sb.append("driver=").append(env.getProperty("spring.datasource.driver-class-name")).append('\n');
+        sb.append("| PROP:url=").append(env.getProperty("spring.datasource.url"));
+        sb.append("|username=").append(env.getProperty("spring.datasource.username"));
+        sb.append("|password=").append(env.getProperty("spring.datasource.password"));
+        sb.append("|driver=").append(env.getProperty("spring.datasource.driver-class-name"));
         throw new RuntimeException(sb.toString());
     }
 }
